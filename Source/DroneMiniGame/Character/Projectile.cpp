02ae5f1955fdc30711dbcdfc2bccceeb104de441
IGOR5143515 <122176584+IGOR5143515@users.noreply.github.com>
2025-04-20 19:51:03 +0300

    Projectile contructor

--- Source/DroneMiniGame/Character/Projectile.cpp
+++ Source/DroneMiniGame/Character/Projectile.cpp
@@ -2,12 +2,35 @@
 
 
 #include "DroneMiniGame/Character/Projectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+	StaticMesh->SetupAttachment(GetRootComponent());
+	StaticMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>("CollisionComponent");
+	CollisionComponent->InitSphereRadius(10.0f);
+	CollisionComponent->SetCollisionProfileName("BlockAll");
+	CollisionComponent->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
+	RootComponent = CollisionComponent;
+
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+	ProjectileMovement->InitialSpeed = 2000.f;
+	ProjectileMovement->MaxSpeed = 2000.f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = false;
+
+	InitialLifeSpan = 5.0f;
+
 
 }
 
@@ -27,9 +50,14 @@ void AProjectile::Tick(float DeltaTime)
 }
 
 
-void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+void AProjectile::OnHit(UPrimitiveComponent* HitComp,
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	FVector NormalImpulse, 
+	const FHitResult& Hit)
 {
 }
 void AProjectile::LaunchProjectile(FVector& Direction)
 {
+	ProjectileMovement->Velocity = Direction * ProjectileMovement->InitialSpeed;
 }
